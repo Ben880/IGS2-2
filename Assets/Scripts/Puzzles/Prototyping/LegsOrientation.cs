dc2c6a049de9a3b18e1658bdd48f8d477d2e3b46
Ben880 <Ben880@users.noreply.github.com>
2020-05-07 17:08:16 -0400

    Legs

---
+++ Assets/Scripts/Puzzles/Prototyping/LegsOrientation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegsOrientation : MonoBehaviour
+{
+
+    public GameObject cameraObj;
+    public Vector3 offsetRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraObj = GameObject.FindWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 eulerRotation = new Vector3(
+            0 + offsetRotation.x, 
+            cameraObj.transform.eulerAngles.y+offsetRotation.y, 
+            0 + offsetRotation.z
+            );
+        transform.rotation = Quaternion.Euler(eulerRotation);
+    }
+}
---
+++ Assets/Scripts/Tools/OnButtonSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(AudioSource))]
+public class OnButtonSound : MonoBehaviour
+{
+    private AudioSource ac;
+
+    void Start()
+    {
+        ac = GetComponent<AudioSource>();
+        GetComponent<HoverButton>().onButtonDown.AddListener(playSound);
+    }
+
+    public void playSound(Hand h)
+    {
+        ac.Stop();
+        ac.Play();
+    }
+    
+}
